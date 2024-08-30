a4f5fcb3116072cd4aaff2daf9c9eae31dcc0208
Vinícius Reis <viniciusreis3105@gmail>
2024-08-30 11:43:11 -0300

    feat: add vercel analytics in project

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const roboto = Roboto({
@@ -31,6 +32,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
         <Footer />
         <Toaster />
       </body>
