977eca2cefae5035257b8fcdc7013e91cd33bc98
unknown <viniciusreis3105@gmail.com>
2024-09-03 13:14:15 -0300

    feat: change sitemap.ts content for deploy

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,55 @@
-import { db } from "@/lib/prisma";
-import { Category, Establishment } from "@prisma/client";
+// import { Category, Establishment } from "@prisma/client";
+// import { MetadataRoute } from "next";
+
+// interface SiteMapProps {
+//   establishment: Establishment;
+//   category: Category;
+// }
+
+// export default function sitemap({
+//   category,
+//   establishment,
+// }: SiteMapProps): MetadataRoute.Sitemap {
+//   return [
+//     {
+//       url: "https://agenda-pgm.vercel.app/",
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 1,
+//     },
+//     {
+//       url: "https://agenda-pgm.vercel.app/categories",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+//     {
+//       url: `https://agenda-pgm.vercel.app/categories/${category.slug}`,
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 1,
+//     },
+//     {
+//       url: "https://agenda-pgm.vercel.app/establishment",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.8,
+//     },
+//     {
+//       url: `https://agenda-pgm.vercel.app/establishment/${establishment.slug}`,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+//     {
+//       url: "https://agenda-pgm.vercel.app/suggestions",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+//   ];
+// }
+import { db } from "@/lib/prisma"; // Certifique-se de ajustar o caminho de importação
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -10,40 +60,40 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: "https://agenda-pgm.vercel.app/",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly" as const, // Ajuste aqui
       priority: 1,
     },
     {
       url: "https://agenda-pgm.vercel.app/establishment",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly" as const, // Ajuste aqui
       priority: 1,
     },
     {
       url: "https://agenda-pgm.vercel.app/categories",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "monthly" as const, // Ajuste aqui
       priority: 0.8,
     },
     {
       url: "https://agenda-pgm.vercel.app/suggestions",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "monthly" as const, // Ajuste aqui
       priority: 0.8,
     },
   ];
 
   const dynamicRoutes = [
-    ...establishments.map((establishment: Establishment) => ({
+    ...establishments.map((establishment) => ({
       url: `https://agenda-pgm.vercel.app/establishment/${establishment.slug}`,
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly" as const, // Ajuste aqui
       priority: 1,
     })),
-    ...categories.map((category: Category) => ({
+    ...categories.map((category) => ({
       url: `https://agenda-pgm.vercel.app/categories/${category.slug}`,
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly" as const, // Ajuste aqui
       priority: 1,
     })),
   ];
